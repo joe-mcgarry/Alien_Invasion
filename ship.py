bbd36b2485027b52ceaac5cb176afd9224a0f4a2
Joe McGarry <joewmcgarry@gmail.com>
2022-09-14 14:03:25 +0100

    Changes ship speed, sets Ship's update method to use ship's x value not rect's

--- ship.py
+++ ship.py
@@ -5,13 +5,16 @@ class Ship:
 
     def __init__(self, game) -> None:
         """Initialise the ship and set the starting position"""
-        self.screen = game.screen 
+        self.screen = game.screen
+        self.settings = game.settings
         self.screen_rect = game.screen.get_rect() #Access screen's rect properties
 
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
-
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Decimal value for ship's horizontal position
+        self.x = float(self.rect.x)
 
         # Movement Flag
         self.moving_right = False
@@ -19,10 +22,15 @@ class Ship:
     
     def update(self):
         """Updates ship's position based on movement flag"""
+        # Don't use elif because right key will take priority
+        # Updating ship's x value, not rect's 
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        
+        # Updates rect object from self.x
+        self.rect.x = self.x
     
     def blitme(self):
         """Draw the ship at its current location"""

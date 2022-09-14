78a17caee4aebbdaecec8e882193c4a8dc77675a
Joe McGarry <joewmcgarry@gmail.com>
2022-09-14 12:49:20 +0100

    Adds Settins class, refactors screen size and background colour

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,8 @@
+import imp
 import sys
 import pygame
+from settings import Settings
+
 
 class AlienInvasion:
     """Class to manage game assets and behaviour"""
@@ -7,24 +10,27 @@ class AlienInvasion:
     def __init__(self):
         """Initialise and create resources"""
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        self.bg_color = (230, 230, 230)
-    
+
     def run_game(self):
         """Start the main game loop"""
         while True:
-            #Watch for keyboard and mouse events
+            # Watch for keyboard and mouse events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            
-            self.screen.fill(self.bg_color)
-            
-            #Make most recently drawn screen visible
+
+            self.screen.fill(self.settings.bg_color)
+
+            # Make most recently drawn screen visible
             pygame.display.flip()
-        
+
+
 if __name__ == '__main__':
-    #Make instance of game and run it
+    # Make instance of game and run it
     ai = AlienInvasion()
     ai.run_game()

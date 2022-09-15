a6ec8b17081bc2a64f840aa86c4d56326fd1dccd
Joe McGarry <joewmcgarry@gmail.com>
2022-09-15 15:17:14 +0100

    Adds high score functionality

--- game_stats.py
+++ game_stats.py
@@ -7,6 +7,7 @@ class GameStats:
         self.reset_stats()
 
         self.game_active = False
+        self.high_score = 0
     
     def reset_stats(self):
         """Initialise stats that change during the game"""

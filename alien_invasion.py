b98b174f23e7dd500f5622370eed2faf4a0a8bc0
Joe McGarry <joewmcgarry@gmail.com>
2022-09-15 13:33:56 +0100

    Button only responds if game is inactive

--- alien_invasion.py
+++ alien_invasion.py
@@ -177,7 +177,8 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         """Start a new game when play button clicked"""
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
             self.stats.reset_stats()
             self.stats.game_active = True
 

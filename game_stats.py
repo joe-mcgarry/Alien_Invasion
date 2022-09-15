9aac53df410e5bd87af89b02b75b50a9076aa443
Joe McGarry <joewmcgarry@gmail.com>
2022-09-15 13:22:51 +0100

    Creates button class and gives it functionality to create message and put text onto to button

--- game_stats.py
+++ game_stats.py
@@ -6,7 +6,7 @@ class GameStats:
         self.settings = game.settings
         self.reset_stats()
 
-        self.game_active = True
+        self.game_active = False
     
     def reset_stats(self):
         """Initialise stats that change during the game"""

263fe3fc07dc09ba095d9d2a61d31b078fac158b
Joe McGarry <joewmcgarry@gmail.com>
2022-09-15 11:21:11 +0100

    Sets speed of aliens and updates position in main game file

--- alien.py
+++ alien.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame.sprite import Sprite
 
+
 class Alien(Sprite):
     """A class to represent one alien in the alien fleet"""
 
@@ -8,6 +9,7 @@ class Alien(Sprite):
         """Initialise the alien and set its starting point"""
         super().__init__()
         self.screen = game.screen
+        self.settings = game.settings
 
         # Load the alien image and set its rect attribute
         self.image = pygame.image.load('images/alien.bmp')
@@ -19,3 +21,8 @@ class Alien(Sprite):
 
         # Store alien's exact horizontal position
         self.x = float(self.rect.x)
+
+    def update(self):
+        """Move the alien to the right"""
+        self.x += self.settings.alien_speed
+        self.rect.x = self.x
--- alien_invasion.py
+++ alien_invasion.py
@@ -36,6 +36,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
@@ -108,6 +109,10 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    def _update_aliens(self):
+        """Updates the position of all aliens in the fleet"""
+        self.aliens.update()
 
     def _update_screen(self):
         """Update images on the screen, flip to new screen"""
